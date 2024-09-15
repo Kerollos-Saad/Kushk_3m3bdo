41975f704076071217fa5fd3fed5a1c57dee2130
Kerollos-Saad <kirloussaad7788@gmail.com>
2024-09-15 21:08:07 +0300

    Add Queries to Generic Repository

--- Kushl_3m3bdo/Data/Repository/GenericRepository.cs
+++ Kushl_3m3bdo/Data/Repository/GenericRepository.cs
@@ -1,4 +1,7 @@
-﻿using Kushl_3m3bdo.Data.Repository.IRepository;
+﻿using System.Linq.Expressions;
+using Kushl_3m3bdo.Data.Repository.IRepository;
+using Kushl_3m3bdo.Models.Consts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kushl_3m3bdo.Data.Repository
 {
@@ -14,6 +17,186 @@ namespace Kushl_3m3bdo.Data.Repository
 		public T GetById(int id)
 		{
 			return _context.Set<T>().Find(id);
+		}
+
+		public async Task<T> GetByIdAsync(int id)
+		{
+			return await _context.Set<T>().FindAsync(id);
+		}
+
+		public IEnumerable<T> GetAll()
+		{
+			return _context.Set<T>().ToList();
+		}
+
+		public async Task<IEnumerable<T>> GetAllAsync()
+		{
+			return await _context.Set<T>().ToListAsync();
+		}
+
+		public T FindExpression(Expression<Func<T, bool>> Filter, string[] includeProperties = null)
+		{
+			IQueryable<T> query = _context.Set<T>();
+
+			if(includeProperties != null)
+				foreach (var property in includeProperties)
+					query = query.Include(property);
+			
+			return query.SingleOrDefault(Filter);
+		}
+
+		public async Task<T> FindExpressionAsync(Expression<Func<T, bool>> Filter, string[] includeProperties = null)
+		{
+			return await _context.Set<T>().SingleOrDefaultAsync(Filter);
+		}
+
+		public IEnumerable<T> FindAllExpression(Expression<Func<T, bool>> Filter, string[] includeProperties = null)
+		{
+			IQueryable<T> query = _context.Set<T>();
+
+			if (includeProperties != null)
+				foreach (var property in includeProperties)
+					query = query.Include(property);
+
+			return query.Where(Filter).ToList();
+		}
+
+		public async Task<IEnumerable<T>> FindAllExpressionAsync(Expression<Func<T, bool>> Filter, string[] includeProperties = null)
+		{
+			return await _context.Set<T>().Where(Filter).ToListAsync();
+		}
+
+		public IEnumerable<T> FindAllExpression(Expression<Func<T, bool>> Filter, int skip, int take)
+		{
+			return _context.Set<T>().Where(Filter).Skip(skip).Take(take).ToList();
+		}
+
+		public async Task<IEnumerable<T>> FindAllExpressionAsync(Expression<Func<T, bool>> Filter, int skip, int take)
+		{
+			return await _context.Set<T>().Where(Filter).Skip(skip).Take(take).ToListAsync();
+		}
+
+		public IEnumerable<T> FindAll(Expression<Func<T, bool>>? Filter, int? skip, int? take,
+			string[] includeProperties = null,
+			Expression<Func<T, object>> orderBy = null, string orderByDirection = OrderBy.Ascending)
+		{
+			IQueryable<T> query = _context.Set<T>();
+
+			if (includeProperties != null)
+				foreach (var property in includeProperties)
+					query = query.Include(property);
+
+			if (Filter!= null)
+				query = query.Where(Filter);
+
+			if(take.HasValue)
+				query = query.Take(take.Value);
+
+			if (skip.HasValue)
+				query = query.Skip(skip.Value);
+
+			if (orderBy != null)
+			{
+				if(orderByDirection ==  OrderBy.Ascending)
+					query = query.OrderBy(orderBy);
+				else
+					query = query.OrderByDescending(orderBy);
+			}
+
+			return query.ToList();
+		}
+
+		public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> Filter, int? skip, int? take, string[] includeProperties = null,
+			Expression<Func<T, object>> orderBy = null, string orderByDirection = OrderBy.Ascending)
+		{
+			IQueryable<T> query = _context.Set<T>();
+
+			if (includeProperties != null)
+				foreach (var property in includeProperties)
+					query = query.Include(property);
+
+			if (Filter != null)
+				query = query.Where(Filter);
+
+			if (take.HasValue)
+				query = query.Take(take.Value);
+
+			if (skip.HasValue)
+				query = query.Skip(skip.Value);
+
+			if (orderBy != null)
+			{
+				if (orderByDirection == OrderBy.Ascending)
+					query = query.OrderBy(orderBy);
+				else
+					query = query.OrderByDescending(orderBy);
+			}
+
+			return await query.ToListAsync();
+		}
+
+		public T Add(T entity)
+		{
+			_context.Set<T>().Add(entity);
+			_context.SaveChanges();
+
+			return entity;
+		}
+
+		public async Task<T> AddAsync(T entity)
+		{
+			await _context.Set<T>().AddAsync(entity);
+			await _context.SaveChangesAsync();
+
+			return entity;
+		}
+
+		public IEnumerable<T> AddRange(IEnumerable<T> entities)
+		{
+			_context.Set<T>().AddRange(entities);
+			_context.SaveChanges();
+
+			return entities;
+		}
+
+		public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+		{
+			await _context.Set<T>().AddRangeAsync(entities);
+			await _context.SaveChangesAsync();
+
+			return entities;
+		}
+
+		public T Remove(T entity)
+		{
+			_context.Set<T>().Remove(entity);
+			_context.SaveChanges();
+
+			return entity;
+		}
+
+		public async Task<T> RemoveAsync(T entity)
+		{
+			_context.Set<T>().Remove(entity);
+			await _context.SaveChangesAsync();
+
+			return entity;
+		}
+
+		public IEnumerable<T> RemoveRange(IEnumerable<T> entities)
+		{
+			_context.Set<T>().RemoveRange(entities);
+			_context.SaveChanges();
+
+			return entities;
+		}
+
+		public async Task<IEnumerable<T>> RemoveRangeAsync(IEnumerable<T> entities)
+		{
+			_context.Set<T>().RemoveRange(entities);
+			await _context.SaveChangesAsync();
+
+			return entities;
 		}
 	}
 }
--- Kushl_3m3bdo/Data/Repository/IRepository/IGenericRepository.cs
+++ Kushl_3m3bdo/Data/Repository/IRepository/IGenericRepository.cs
@@ -1,7 +1,53 @@
-﻿namespace Kushl_3m3bdo.Data.Repository.IRepository
+﻿using System.Linq.Expressions;
+using Kushl_3m3bdo.Models.Consts;
+
+namespace Kushl_3m3bdo.Data.Repository.IRepository
 {
 	public interface IGenericRepository<T> where T : class
 	{
 		T GetById(int id);
+
+		Task<T> GetByIdAsync(int id);
+
+		IEnumerable<T> GetAll();
+
+		Task<IEnumerable<T>> GetAllAsync();
+
+		T FindExpression(Expression<Func<T, bool>> Filter, string[] includeProperties = null);
+
+		Task<T> FindExpressionAsync(Expression<Func<T, bool>> Filter, string[] includeProperties = null);
+
+		IEnumerable<T> FindAllExpression(Expression<Func<T, bool>> Filter, string[] includeProperties = null);
+
+		Task<IEnumerable<T>> FindAllExpressionAsync(Expression<Func<T, bool>> Filter,
+			string[] includeProperties = null);
+
+		IEnumerable<T> FindAllExpression(Expression<Func<T, bool>> Filter, int skip, int take);
+
+		Task<IEnumerable<T>> FindAllExpressionAsync(Expression<Func<T, bool>> Filter, int skip, int take);
+
+		IEnumerable<T> FindAll(Expression<Func<T, bool>> Filter, int? skip, int? take,
+			string[] includeProperties = null,
+			Expression<Func<T, object>> orderBy = null, string orderByDirection = OrderBy.Ascending);
+
+		Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> Filter, int? skip, int? take,
+			string[] includeProperties = null,
+			Expression<Func<T, object>> orderBy = null, string orderByDirection = OrderBy.Ascending);
+
+		T Add(T entity);
+
+		Task<T> AddAsync(T entity);
+
+		IEnumerable<T> AddRange(IEnumerable<T> entities);
+
+		Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+
+		T Remove(T entity);
+
+		Task<T> RemoveAsync(T entity);
+
+		IEnumerable<T> RemoveRange(IEnumerable<T> entities);
+
+		Task<IEnumerable<T>> RemoveRangeAsync(IEnumerable<T> entities);
 	}
 }
